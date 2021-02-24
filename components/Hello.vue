eab17aabbf6097a99640610a939401c3680541e3
kenxdrgn <61741059+kenxdrgn@users.noreply.github.com>
2021-02-24 16:54:45 +0900

    restore version

--- components/Hello.vue
+++ components/Hello.vue
@@ -2,7 +2,43 @@
   <div
     class="py-60 text-center font-medium tracking-wide text-5xl md:text-8xl dark:text-gray-300"
   >
-    <nuxt-link to="/about" aria-label="About" title="About"> HELLO </nuxt-link>
+    <nuxt-link to="/about" aria-label="About" title="About">
+      <vue-typed-js
+        class="flex justify-center"
+        :loop="true"
+        :type-speed="50"
+        :back-speed="50"
+        :strings="[
+          'Hello',
+          'こんにちは',
+          '你好',
+          'Hola',
+          'Bonjour',
+          '哈囉',
+          'नमस्ते',
+          'Hallo',
+          'Здравствуйте',
+          'Olá',
+          'Ciao',
+          '여보세요',
+          'Merhaba',
+          'مرحبا',
+          'สวัสดี',
+          'Hallå',
+          'Hej',
+          'xin chào',
+          'dzień dobry',
+          'Hei',
+          'שלום',
+          'Χαίρετε',
+          'Salut',
+          'Ahoj',
+          'zdravo',
+        ]"
+      >
+        <span class="typing"></span>
+      </vue-typed-js>
+    </nuxt-link>
   </div>
 </template>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     { src: '@/plugins/vue-awesome-swiper', ssr: false },
+    { src: '@/plugins/vue-typed-js', ssr: false },
     { src: '~/plugins/aos', ssr: false },
   ],
 
